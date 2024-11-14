da65800ec3796db5f94d49362c87a9b67a367ee1
eugene-sew <eugenesew4@gmail.com>
2024-11-14 11:30:18 +0000

    clarity

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { clarity } from "react-microsoft-clarity";
 import { Layout } from "./components/Layout";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { Home } from "./pages/Home";
@@ -85,6 +86,10 @@ const router = createBrowserRouter(
 );
 
 function App() {
+  const TOKEN = import.meta.env.VITE_CLARITY_KEY;
+  const CLARITY_ID = import.meta.env.VITE_CLARITY_ID;
+  if (process.env.NODE_ENV === "production") clarity.init(CLARITY_ID);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
